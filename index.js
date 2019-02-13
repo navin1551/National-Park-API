9b4f57823ee3bad826981b320e089d7030432859
Navin Kumar <navin1551@gmail.com>
2019-02-13 13:43:55 -0800

    done except for error message and HTML formatting

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ function searchButtonHandle() {
     $('#search-form').on('submit', event => {
         event.preventDefault();
         const stateSearch = $('#state-select').val();
-        const numLimit = $('#limit-select').val();
-        getParkInfo(stateSearch,numLimit);
-        $('.results-area').show();
+        const numLimit = $('#limit-select').val() -1;
+        getParkInfo(stateSearch, numLimit);
     })
 }
-
-
 
 
 
 //function to construct query parameters for URL
 const apiKey = 'EeDOxICEgmJhZSYmhjtvagsr3uuF1w0ZlFs5xn9X';
 
-const baseUrl = 'http://api.nps.gov/api/v1/parks';
+const baseUrl = 'https://api.nps.gov/api/v1/parks';
 
 function formatQueryParams(params){
     const queryItemsConstruct = Object.keys(params)
@@ -27,13 +24,13 @@ function formatQueryParams(params){
 }
 
 //function to retrieve data using parameters 
-function getParkInfo(query, limit=10) {
+function getParkInfo(query, limit) {
     const params = {
-        api_key: apiKey,
         q: query,
+        key: apiKey,
         limit
     };
-    const queryString = formatQueryParams(params)
+    const queryString = formatQueryParams(params);
     const url = baseUrl + '?' + queryString;
 
     fetch(url)
@@ -43,25 +40,33 @@ function getParkInfo(query, limit=10) {
             }
             throw new Error(response.statusText);
         })
-        .then(reponseJson => console.log(JSON.stringify(reponseJson)))
+        .then(responseJson => {
+            console.log(responseJson) 
+            displayResults(responseJson)
+        })
         .catch(err => {
-            $('#error-message').text('Not found');
+            $('#error-message').text('NOT FOUND');
         });
 }
 
 
 
+//function to display search results to DOM
+function displayResults(responseJson) {
+    $('.results-list').empty();
+    if (responseJson.data.length > 0) {
+        for (let i = 0; i < responseJson.data.length; i++) {
+            $('.results-list').append(
+                `<li><h3>${responseJson.data[i].fullName}</h3>
+                <p>${responseJson.data[i].description}</p>
+                <a href= "${responseJson.data[i].url}" target="_blank">Link</a>
+                </li>`);
+        $('.results-area').show();
+    }}
+    else{
+        $('#error-message').text('RESULTS NOT FOUND');
+    }
+   };
 
 
-
-
-/*function to display search results to DOM
-function displayResults() {
-    $('#search-form').empty();
-    for (let i = 0; i < )
-}*/
-
-
-
-
-$(searchButtonHandle);
+$(searchButtonHandle)
